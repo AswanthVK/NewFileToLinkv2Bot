9c174f08a19d0180ca839c22cf45a8297e8d998a
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-08-01 19:41:11 +0530

    Update forcesub.py

--- WebStreamer/bot/plugins/forcesub.py
+++ WebStreamer/bot/plugins/forcesub.py
@@ -1,67 +1,47 @@
-# (c) @AbirHasan2005
+import logging
+logger = logging.getLogger(__name__)
 
-import asyncio
 from WebStreamer.bot import StreamBot
 from WebStreamer.vars import Var
-from pyrogram import Client
-from pyrogram.errors import FloodWait, UserNotParticipant
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied
 
 
-async def ForceSub(bot: Client, event: Message):
-    """
-    Custom Pyrogram Based Telegram Bot's Force Subscribe Function by @AbirHasan2005.
-    If User is not Joined Force Sub Channel Bot to Send a Message & ask him to Join First.
-    
-    :param bot: Pass Client.
-    :param event: Pass Message.
-    :return: It will return 200 if Successfully Got User in Force Sub Channel and 400 if Found that User Not Participant in Force Sub Channel or User is Kicked from Force Sub Channel it will return 400. Also it returns 200 if Unable to Find Channel.
-    """
-    
-    try:
-        invite_link = await bot.create_chat_invite_link(chat_id=(int(Var.UPDATES_CHANNEL) if Var.UPDATES_CHANNEL.startswith("-100") else Var.UPDATES_CHANNEL))
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, event)
-        return fix_
-    except Exception as err:
-        print(f"Unable to do Force Subscribe to {var.UPDATES_CHANNEL}\n\nError: {err}\n\nContact Support Group: https://t.me/NewBotzSupport")
-        return 200
-    try:
-        user = await bot.get_chat_member(chat_id=(int(Var.UPDATES_CHANNEL) if Var.UPDATES_CHANNEL.startswith("-100") else Var.UPDATES_CHANNEL), user_id=event.from_user.id)
-        if user.status == "kicked":
-            await bot.send_message(
-                chat_id=event.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/NewBotzSupport).",
-                parse_mode="markdown",
-                disable_web_page_preview=True,
-                reply_to_message_id=event.message_id
-            )
-            return 400
-        else:
-            return 200
-    except UserNotParticipant:
-        await bot.send_message(
-            chat_id=event.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
-                    ],
-                    [
-                        InlineKeyboardButton("🔄 Refresh", url=f"https://t.me/NewFileToLinkv2Bot?start")
-                    ]
-                ]
-            ),
-            parse_mode="markdown",
-            reply_to_message_id=event.message_id
-        )
-        return 400
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, event)
-        return fix_
-    except Exception as err:
-        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}\n\nContact Support Group: https://t.me/NewBotzSupport")
-        return 200
+@Client.on_message(filters.private & filters.incoming)
+async def force_sub(c, m):
+    if Var.UPDATES_CHANNEL:
+        try:
+            chat = await c.get_chat_member(Var.UPDATES_CHANNEL, m.from_user.id)
+            if chat.status=='kicked':
+                return await m.reply_text('Hai you are kicked from my updates channel. So, you are not able to use me',  quote=True)
+
+        except UserNotParticipant:
+            button = [[
+                InlineKeyboardButton('join Updates channel', url=f'https://t.me/{Var.UPDATES_CHANNEL}')
+                ],[
+                InlineKeyboardButton('🔄 Refresh 🔄', url=f'https://t.me/NewFileToLinkv2Bot?start')
+            ]]
+            markup = InlineKeyboardMarkup(button)
+            return await m.reply_text(text="Hey join in my updates channel to use me.", parse_mode='markdown', reply_markup=markup, quote=True)
+
+        except ChatAdminRequired:
+            logger.warning(f"Make me admin in @{Var.UPDATES_CHANNEL}")
+            if m.from_user.id in Config.AUTH_USERS:
+                return await m.reply_text(f"Make me admin in @{Var.UPDATES_CHANNEL}")
+
+        except UsernameNotOccupied:
+            logger.warning("The forcesub username was Incorrect. Please give the correct username.")
+            if m.from_user.id in Var.OWNER_ID:
+                return await m.reply_text("The forcesub username was Incorrect. Please give the correct username.")
+
+        except Exception as e:
+            if "belongs to a user" in str(e):
+                logger.warning("Forcesub username must be a channel username Not yours or any other users username")
+                if m.from_user.id in Var.OWNER_ID:
+                    return await m.reply_text("Forcesub username must be a channel username Not yours or any other users username")
+            logger.error(e)
+            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [Owner](https://t.me/AswanthVK)", disable_web_page_preview=True, quote=True)
+
+    await m.continue_propagation()
+
