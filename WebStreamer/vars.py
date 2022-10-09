cd7cdd4ed2661ccd7dd5da93b00b622358024866
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-10-09 18:17:41 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -8,13 +8,13 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+    API_ID = int(environ.get("API_ID", "663122"))
+    API_HASH = str(environ.get("API_HASH", "23dac54b523173b5f83014ae566584bd"))
+    BOT_TOKEN = str(environ.get("BOT_TOKEN", "5309488011:AAEycqFwT5-fyeev7dgjJXclmrjCde_4OQs"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
+        environ.get("BIN_CHANNEL", "-1001243844776")
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
@@ -40,7 +40,7 @@ class Var(object):
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
 
-    UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "aredirect"))
-    OWNER_ID = int(environ.get('OWNER_ID', '777000'))
+    UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "NewBotz"))
+    OWNER_ID = int(environ.get('OWNER_ID', '662933911'))
 
-    BANNED_CHANNELS = list(set(int(x) for x in str(environ.get("BANNED_CHANNELS", "-1001296894100")).split()))
+    BANNED_CHANNELS = list(set(int(x) for x in str(environ.get("BANNED_CHANNELS", "")).split()))
